12212f0130d568a8160d260ce2c6321cacbf80c3
Kleinas Emilis <emilis.kleinas@mif.stud.vu.lt>
2024-10-12 12:42:08 +0300

    added query client

---
+++ src/app/components/DataDisplay.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { allSpells } from '@/api/utils/queryKeyFactory';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react'
+import LoadingPage from '../pages/LoadingPage';
+import { getLuciferQuote } from '@/api/lucifer/getSpells';
+
+const DataDisplay = () => {
+    console.log(allSpells)
+  const {data, isLoading} = useQuery({queryKey: [allSpells], queryFn: getLuciferQuote });
+  if (isLoading) {
+    <LoadingPage />
+  }
+
+  return (
+    <div>
+        {`${data}`}
+    </div>
+  )
+}
+
+export default DataDisplay
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import DataDisplay from "./components/DataDisplay";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import ErrorPage from "./pages/ErrorPage";
+
 export default function Home() {
+  const [queryClient] = useState(() => new QueryClient({defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    }
+  }}))
+
   return (
-    <section>
-      <center>
-        <h1>
-          Hello from bikgud
-        </h1>
-      </center>
-    </section>
+        <QueryClientProvider client={queryClient}>
+          <ErrorBoundary errorComponent={ErrorPage}>
+          <div>
+            <DataDisplay />
+          </div>
+          </ErrorBoundary>
+        </QueryClientProvider>
   );
 }
